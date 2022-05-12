8bac9006ba9a142ef1b71bd194566941306aba7f
Palmer Nelson <pfnelson@smu.edu>
2022-05-11 17:47:58 -0700

    j

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Dojo! </h1>
+      <ul>
+        <li>Learn React </li>
+        <li>Climb Mount Everrest</li>
+        <li>Runa a marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
